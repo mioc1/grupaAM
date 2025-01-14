9e5ba6caa5b2dcf4756cd03f3b6f732aaabaf4b3
Albert <albimioc@gmail.com>
2025-01-14 22:58:16 +0100

    Prvi commit

---
+++ BookShop/src/pages/IndexPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Provjera ishoda 1 i 2</h1>
+  </div>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+  </q-page>
+</template>
+
+<script setup>
+//
+</script>
